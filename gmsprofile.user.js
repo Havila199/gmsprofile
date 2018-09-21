5bbba30147ab89c0aabc575d21a22cc3709c26e6
Havila <Havila2k@gmail.com>
2018-09-21 16:29:45 +0100

    oofie

--- gmsprofile.user.js
+++ gmsprofile.user.js
@@ -1,9 +1,9 @@
 	// ==UserScript==
 	// @name         gmodstore Checker
-	// @namespace    meharryp.xyz
+	// @namespace    gmodstore.com
 	// @version      1.0.0
 	// @description  View a gmodstore user's information on their steam profile page.
-	// @author       nobody
+	// @author       nobody & meharryp
 	// @downloadURL  https://raw.githubusercontent.com/Havila199/gmsprofile/master/gmsprofile.user.js
 	// @match        *://steamcommunity.com/id/*
 	// @match        *://steamcommunity.com/profiles/*
@@ -21,106 +21,83 @@
 		var data = {};
 		
 		if (!apikey || apikey == ""){
-			alert("SFProfile: You haven't set an API key! Edit the script file and add one to keep using this.");
+			alert("GMSProfile: You haven't set an API key! Edit the script file and add one to keep using this.");
 		}
-
 		$(".profile_leftcol").prepend(`
-			<div class="profile_customization">
-				<div class="profile_customization_header ellipsis">gmodstore Details</div>
-				<div class="profile_customization_block">
-					<div class="customtext_showcase">
-						<div class="showcase_content_bg showcase_notes" id="sfholder"></div>
+			<div class="profile_customization" style="background-image: none;background: #d9534f;">
+				<div class="profile_customization_header ellipsis" style="background: #d9534f;color: #f5f8fa;font-size: 1.1rem;font-weight: 500;">Gmodstore Bans</div>
+				<div class="profile_customization_block" style="height: auto;width: 100%;z-index: -1;">
+					<div class="customtext_showcase" style="background-color:rgba(255,255,255,0.2);">
+						<div class="showcase_content_bg showcase_notes" id="gmsholder2"></div>
 					</div>
 				</div>
 			</div>
 		`);
-
+		$(".profile_leftcol").prepend(`
+			<div class="profile_customization" style="background-image: none;background: #3097d1;">
+				<div class="profile_customization_header ellipsis" style="background: #3097d1;color: #f5f8fa;font-size: 1.1rem;font-weight: 500;">Gmodstore Details</div>
+				<div class="profile_customization_block" style="height: auto;width: 100%;z-index: -1;">
+					<div class="customtext_showcase" style="background-color:rgba(255,255,255,0.2);">
+						<div class="showcase_content_bg showcase_notes" id="gmsholder"></div>
+					</div>
+				</div>
+			</div>
+		`);
 		var steamid = g_rgProfileData.steamid;
-
-			// Get users bans
-		GM_xmlhttpRequest({
-			url: "https://gmodstore.com/api/users/banned/" + steamid + "?api_key=" + apikey,
-			method: "GET",
-			onload: function(res){
-			        console.log(res.responseText);
-				bans = JSON.parse(res.responseText).bans;
-
-				console.log(bans);
 
 				// Get user data
 				GM_xmlhttpRequest({
-					url: "https://gmodstore.com/api/users/search/steam64/" + steamid + "?api_key=" + apikey,
+					url: "https://api.gmodstore.com/v2/users/" + steamid +"?with=bans,group",
 					method: "GET",
+                    headers: {
+                        "authorization": "Bearer " + apikey,
+                        "cache-control": "no-cache",
+                    },
 					onload: function(res){
 					data = JSON.parse(res.responseText);
 
 	                        	var html = `
-						<b>Reputation: </b>${parseInt(data.user.scripts_rep) + parseInt(data.user.jobs_rep)}<br>
-						<b>Registered </b>${$.timeago(new Date(parseInt(data.user.dateRegistered) * 1000))}<br>
-						<b>Last online </b>${$.timeago(new Date(parseInt(data.user.lastAction) * 1000))}<br>
-						<b>Country: </b>${data.user.country_code}<br>
-						<b>Usergroup: </b>${(data.user.usergroup == "0" && "User" || data.user.usergroup == "1" && "Developer" || data.user.usergroup == "2" && "Moderator" || data.user.usergroup == "3" && "Admin")}<br>`;
+                        <div class="" style="color:white;">
+						<b>Username: </b>${data.data.name}<br>
+						<b>Country: </b>${data.data.country_code}<br>
+						<b>Rank: </b>${data.data.group.title}<br>
+						<b>Reputation: </b>SOON™<br>
+						<b>Registered: </b>SOON™<br>
+						<b>Online: </b>SOON™<br>
+                        </div>
+                           `;
 
-					if (bans.length > 0){
-						for (var i=0; i < bans.length; i++){
-							var ban = bans[i];
-							html += `
-								<br>
-								<b style="color:red">Banned ${$.timeago(new Date(parseInt(ban.ban_starttime) * 1000))}, ban ends in ${(parseInt(ban.ban_endtime) !== 0 && $.timeago(new Date(ban.ban_endtime)) || "Never")}.</b><ul>
-								<li><i>Reason:</i> ${ban.ban_reason}<br></li>
-								</ul><i>Banned from:</i><ul>`;
 
-							for (var i in ban.attributes){
-								if (parseInt(ban.attributes[i])){
-	                        		html += `<li>`;
-									switch (i){
-										case "ban_create_appeal":
-											html += "Creating ban appeals";
-											break;
-										case "ban_everything":
-											html += "Everything";
-											break;
-										case "ban_forum_posting":
-											html += "Posting on the forums";
-											break;
-										case "ban_job_applying":
-											html += "Applying for jobs";
-											break;
-										case "ban_job_creation":
-											html += "Creating new jobs";
-											break;
-										case "ban_script_creation":
-											html += "Creating new scripts";
-											break;
-										case "ban_script_downloading":
-											html += "Downloading scripts";
-											break;
-										case "ban_script_purchasing":
-											html += "Purchasing scripts";
-											break;
-										case "ban_script_reviewing":
-											html += "Reviewing scripts";
-											break;
-										case "ban_user_reviewing":
-											html += "Reviewing users";
-											break;
-										default:
-											html += "Something";
-											break;
-										}
+	                       html += `<br>
+<div class="header_installsteam_btn header_installsteam_btn_gray" style="color:white;">
+<a class="header_installsteam_btn_content" href="https://gmodstore.com/users/view/${steamid}">Gmodstore Profile</a>
+</div>`;
+							$("#gmsholder").html(html);
+                        if (data.data.bans[0] !== undefined) {
 
-										html += `</li>`;
-									}
-	                            }
+                        var bans = data.data.bans[0];
+                        console.log(bans);
+                        if(bans.end == null){ var banend = "Permanent"} else{ var banend = bans.end};
+                        if(bans.properties == "addon.create"){ var bantype = "Script Creation"};
+                        if(bans.properties == "everything"){ var bantype = "Everything"};
+                        if(bans.properties == "forum.post"){ var bantype = "Forum Posts"};
 
-	                       		html += `</ul>`;
-							}
-						}
-
-	                       html += `<br><a href="https://gmodstore.com/users/view/${steamid}" target="_blank">gmodstore Profile</a>`;
-							$("#sfholder").html(html);
-						}
+                        var html2 = `
+                         <div style="color:white;">
+						<b>Ban Date: </b>${bans.start}<br>
+						<b>Unban Date: </b>${banend}<br>
+						<b>Banned From: </b>${bantype}<br>
+						<b>Reason: </b>${bans.reason}<br>
+                        </div>
+                           `;
+							$("#gmsholder2").html(html2);
+						}else{
+                        var html2 = `
+                         <div style="color:white;">
+						<center><b><h1>NO BANS</h1></b></center>
+                        </div>
+                           `;
+							$("#gmsholder2").html(html2);
+                        }}
 					});
-				}
-			});
 	})();
